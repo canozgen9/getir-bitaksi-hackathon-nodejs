ebbf811259ce454a20fc519dc5c50fc2e1ea47be
Can Özgen <canozgen9@hotmail.com>
2018-02-14 23:13:51 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const bodyParser = require("body-parser");
+const express = require("express");
+const MongoClient = require("mongodb").MongoClient;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express();
+
+const url = "mongodb://dbUser:dbPassword@ds155428.mlab.com:55428/getir-bitaksi-hackathon";
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/searchRecord', (req, res) => {
+    const maxCount = req.body.maxCount;
+const minCount = req.body.minCount;
+const startDate = req.body.startDate;
+const endDate = req.body.endDate;
+
+res.writeHead(200, {"Content-Type": "application/json"});
+
+MongoClient.connect(url, function(err, db) {
+
+    var records = [];
+
+    var cursor = db.collection('records').find({"createdAt": {"$gte": new Date(startDate), "$lt": new Date(endDate)}});
+    cursor.each(function(err, item) {
+        if (item) {
+            var sum = 0;
+            for (var i = 0; i < item.counts.length ; i++) {
+                sum += item.counts[i];
+            }
+
+            if (sum >= minCount && sum <= maxCount) {
+                records.push({key: item.key, createdAt: item.createdAt, totalCounts: sum});
+            }
+        }
+        else {
+            var json = JSON.stringify({
+                code: 0,
+                msg: "Success",
+                records: records
+            });
+            res.end(json);
+        }
+    });
+    db.close();
+});
+});
+
+app.listen(3000, () => {
+    console.log("Started on http://localhost:3000");
+});
